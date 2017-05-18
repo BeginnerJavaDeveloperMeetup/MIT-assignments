93bf0730ed236a678b7e1811c5eaf971dd80f058
Jennifer Hurd <jenhurd8@gmail.com>
2017-05-17 21:54:12 -0500

    Corrected File

--- workspace/MIT_Library/src/Book.java
+++ workspace/MIT_Library/src/Book.java
@@ -1,21 +1,10 @@
 public class Book {
 	String title;
 	boolean borrowed;
-	
-	public static void main(String[] arguments) {
-		// Small test of the Book class
-		Book example = new Book("The Da Vinci Code");
-		System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
-		System.out.println("Borrowed? (should be false): " + example.isBorrowed());
-		example.rented();
-		System.out.println("Borrowed? (should be true): " + example.isBorrowed());
-		example.returned();
-		System.out.println("Borrowed? (should be false): " + example.isBorrowed());
-	}
+
 	// Creates a new Book
 	public Book(String bookTitle) {
 		title = bookTitle;
-		// Implement this method
 	}
 
 	// Marks the book as rented
@@ -32,15 +21,24 @@ public class Book {
 	public boolean isBorrowed() {
 		return borrowed;
 	}
-	
-	public void rented(){
-		borrowed = true;
-	}
 
 	// Returns the title of the book
 	public String getTitle() {
 		return title;
 	}
+	
+	void rented(){
+		borrowed = true;
+	}
 
-
+	public static void main(String[] arguments) {
+		// Small test of the Book class
+		Book example = new Book("The Da Vinci Code");
+		System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
+		System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+		example.rented();
+		System.out.println("Borrowed? (should be true): " + example.isBorrowed());
+		example.returned();
+		System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+	}
 }
